5a80e3df0e224948e3bb907250b5efd1c707497a
Zouhir HANAFI <zouhir.hanafi@gmail.com>
2021-11-08 12:55:14 +0100

    pwa : quick fix bad-precaching-response #288

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
+const __DEV__ = process.env.NODE_ENV === "development";
+
 module.exports = withPWA({
   pwa: {
+    disable: __DEV__,
     dest: 'public',
     runtimeCaching,
+    // quick fix bad-precaching-response #288
+    buildExcludes: [/middleware-manifest.json$/]
   },
 })
