011088fa610147544380ebade4cd3a3994fefc4a
Zouhir HANAFI <zouhir.hanafi@gmail.com>
2021-11-08 16:28:01 +0100

    integration storybook

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-postcss"
+  ],
+  presets: [path.resolve(__dirname, "./next-preset.js")],
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: true,
+        esModuleInterop: true,
+      },
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+}
