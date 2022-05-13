ae8029c01970fe48076d3f2eebe6b976e532cf3c
OmarAsloune <omar.asloune1@gmail.com>
2022-05-13 18:36:36 +0100

    u3

--- src/features/BureauDouane/FormBureauDouane.tsx
+++ src/features/BureauDouane/FormBureauDouane.tsx
@@ -12,7 +12,8 @@ import Mitems from 'widgets/Mitems';
 import { ArchiveIcon, ClipboardListIcon, PencilAltIcon, ReplyIcon, TrashIcon } from "@heroicons/react/outline";
 import axios from "axios";
 
-//zzzzz
+//zzzzzgit 
+//qqqq
 type FormBureauDouaneProps = {
     bureauDouane: Article;
     disable: boolean;
